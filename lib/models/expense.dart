676ac55020112c261b4108329c9272864ef90894
Abhinandan <123541670+ak471909@users.noreply.github.com>
2024-12-13 16:46:52 +0400

    Add files via upload

---
+++ lib/models/expense.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
+// Defines the structure an expense should have
+
+const uuid = Uuid();
+
+// custom type that has a combination of predefined permitted values
+enum Category { food, travel, leisure, work }
+
+// mapping expense categories to their corresponding diplay icons
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  final Category category;
+  final List<Expense> expenses;
+
+  //getter function
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}
